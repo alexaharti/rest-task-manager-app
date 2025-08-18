3077bc2f85926f5a9cbb61965f20f83e1ee69b44
Harti Alexandrina <hartialexandrina@gmail.com>
2025-08-18 10:58:45 +0300

    Implement ResourceNotFoundException

--- src/main/java/pixel/academy/rest_task_manager_app/exception/ResourceNotFoundException.java
+++ src/main/java/pixel/academy/rest_task_manager_app/exception/ResourceNotFoundException.java
@@ -2,4 +2,15 @@ package pixel.academy.rest_task_manager_app.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
 
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
 }
