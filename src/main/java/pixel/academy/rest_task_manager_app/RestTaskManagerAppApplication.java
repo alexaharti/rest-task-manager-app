0d53d571934326ad7a3b72b1b28a4538df4ec09b
Harti Alexandrina <hartialexandrina@gmail.com>
2025-08-18 10:40:54 +0300

    Initial commit

---
+++ src/main/java/pixel/academy/rest_task_manager_app/RestTaskManagerAppApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.rest_task_manager_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTaskManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTaskManagerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pixel/academy/rest_task_manager_app/RestTaskManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.rest_task_manager_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTaskManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
